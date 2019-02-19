39b9a054eec963f734e264ac1423cd5effa24a65
Flavien DELANGLE <flaviendelangle@gmail.com>
2019-02-19 14:48:23 +0100

    APP-3555 Improve form UX (allow render props in FormSection)

--- src/FormSection/FormSection.tsx
+++ src/FormSection/FormSection.tsx
@@ -1,24 +1,23 @@
 import * as React from 'react'
-import { omit, isEmpty } from 'lodash'
+import { omit, isEmpty, isFunction } from 'lodash'
 
 import { StatusContext, SectionContext } from '../contexts'
 import { FormSectionStatus } from './FormSection.interface'
 
-const useSectionStatus = name => {
+const FormSection = ({ name, children }) => {
   const form = React.useContext(StatusContext)
   const [errors, updateErrors] = React.useState({})
 
+  const status: FormSectionStatus = React.useMemo(() => ({
+    hasError: !isEmpty(errors)
+  }), [errors])
+
   React.useEffect(
-    () => {
-      const status: FormSectionStatus = {
-        hasError: !isEmpty(errors)
-      }
+    () => form.setSectionStatus(name, status),
+    [name, status, form.setSectionStatus]
+  )
 
-      form.setSectionStatus(name, status)
-    },
-    [name, errors, form.setSectionStatus])
-
-  return React.useMemo(
+  const sectionStatus = React.useMemo(
     () => ({
       setError: (fieldName: string, error: string) => {
         updateErrors(prevErrors =>
@@ -30,14 +29,14 @@ const useSectionStatus = name => {
     }),
     [updateErrors]
   )
-}
-
-const FormSection = ({ name, children }) => {
-  const sectionStatus = useSectionStatus(name)
 
   return (
     <SectionContext.Provider value={sectionStatus}>
-      { children }
+      {
+        isFunction(children)
+          ? children(status)
+          : children
+      }
     </SectionContext.Provider>
   )
 }
