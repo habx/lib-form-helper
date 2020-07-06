c95a4bc8e3f2c74bddc67c68f956f086360b44de
Flavien DELANGLE <flaviendelangle@gmail.com>
2020-07-06 14:06:45 +0200

    APP-12608 Nested forms are not supported due to name automatic concatenation

--- src/FormSection/FormSection.context.ts
+++ src/FormSection/FormSection.context.ts
@@ -2,7 +2,9 @@ import * as React from 'react'
 
 import { SectionContextProps } from './FormSection.interface'
 
-export default React.createContext<SectionContextProps>({
+export const DEFAULT_SECTION_CONTEXT: SectionContextProps = {
   name: '',
   path: [],
-})
+}
+
+export default React.createContext<SectionContextProps>(DEFAULT_SECTION_CONTEXT)
