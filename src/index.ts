f2f78bc0919a13c8b005116f3f203c8184ab75f7
Flavien DELANGLE <flaviendelangle@gmail.com>
2020-10-19 13:50:21 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ export { default as FormHelperProvider } from './FormHelperProvider'
 export { default as Form, FormContext } from './Form'
 export { default as FormSection } from './FormSection'
 export { default as FormSectionWatcher } from './FormSectionWatcher'
-export { default as withFinalForm } from './withFinalForm'
+export {
+  default as withFinalForm,
+  ValidationCallback,
+  FormatterCallback,
+  ParserCallback,
+} from './withFinalForm'
 export { default as useFinalFormField } from './useFinalFormField'
 export { default as useStatus } from './useStatus'
 
