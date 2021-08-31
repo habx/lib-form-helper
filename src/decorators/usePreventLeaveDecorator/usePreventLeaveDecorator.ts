812a5493f7307ca03030d1478c8936316d4a3312
Alexis Fontaine <alexis.fontaine@outlook.com>
2021-08-31 15:33:24 +0200

    Subscribe to `dirtySinceLastSubmit` state on `usePreventLeaveDecorator`

--- src/decorators/usePreventLeaveDecorator/usePreventLeaveDecorator.ts
+++ src/decorators/usePreventLeaveDecorator/usePreventLeaveDecorator.ts
@@ -4,60 +4,66 @@ import type { useHistory } from 'react-router'
 
 import { confirm } from '@habx/ui-core'
 
-export interface UsePreventLeaveDecoratorOptions {
-  message?: string
-}
 const DEFAULT_MESSAGE = 'Quitter sans sauvegarder les modifications ?'
 
 export const usePreventLeaveDecorator = (
   history?: ReturnType<typeof useHistory>,
   options?: UsePreventLeaveDecoratorOptions
 ) => {
-  const pristineRef = React.useRef(false)
+  const dirtySinceLastSubmitRef = React.useRef(false)
 
   React.useEffect(() => {
     const preventNavigation = (e: BeforeUnloadEvent) => {
-      if (!pristineRef.current) {
+      if (dirtySinceLastSubmitRef.current) {
         e.preventDefault()
         e.returnValue = ''
         return ''
       }
     }
+
     window.addEventListener('beforeunload', preventNavigation, false)
 
-    return () => {
-      window.removeEventListener('beforeunload', preventNavigation)
-    }
+    return () => window.removeEventListener('beforeunload', preventNavigation)
   }, [])
 
   React.useEffect(() => {
     if (!history) {
       return
     }
+
     const unblock = history.block((location) => {
-      if (!pristineRef.current) {
+      if (dirtySinceLastSubmitRef.current) {
         const confirmLeaving = async () => {
           const hasConfirmed = await confirm(
             options?.message ?? DEFAULT_MESSAGE
           )
+
           if (hasConfirmed) {
             unblock()
             history.push(location)
           }
         }
+
         confirmLeaving()
+
         return false
       }
     })
+
     return unblock
   }, [])
+
   return React.useCallback<Decorator<any, any>>((form) => {
     const subscriber: Subscriber<FormState<any>> = (state) => {
-      pristineRef.current = state.pristine
+      dirtySinceLastSubmitRef.current = state.dirtySinceLastSubmit
     }
 
     return form.subscribe(subscriber, {
-      pristine: true,
+      dirtySinceLastSubmit: true,
     })
   }, [])
+}
+
+export interface UsePreventLeaveDecoratorOptions {
+  message?: string
 }
